b69a1e82ece6d555967b5b90ca05f2a4e1d77f83
angelia-yuqi <yuqi-angelia.wang@schneider-electric.com>
2015-06-11 12:52:31 +0800

    ff

--- src/app/actions/DimAction.jsx
+++ src/app/actions/DimAction.jsx
@@ -3,7 +3,7 @@
 import AppDispatcher from '../dispatcher/AppDispatcher.jsx';
 
 import Dim from '../constants/actionType/Dim.jsx';
-import Ajax from '../ajax/ajax.jsx';
+import Ajax from '../ajax/ajax.jsx';   
 
 var Action = Dim.Action;
 
