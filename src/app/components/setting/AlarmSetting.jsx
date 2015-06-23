6f42d7575debdfb62ad73c173643968d017ba31a
angelia-yuqi <yuqi-angelia.wang@schneider-electric.com>
2015-06-23 15:44:30 +0800

    alarmsetting get from sql

--- src/app/components/setting/AlarmSetting.jsx
+++ src/app/components/setting/AlarmSetting.jsx
@@ -1,36 +1,43 @@
 import React from "react";
 import {Route, DefaultRoute, RouteHandler, Link, Navigation, State} from 'react-router';
-import {SvgIcon, IconButton, DropDownMenu, TextField, Dialog, FlatButton, Overlay} from 'material-ui';
+import {Toggle, Checkbox} from 'material-ui';
 import assign from "object-assign";
 
 import AlarmSettingStore from '../../stores/AlarmSettingStore.jsx';
 import AlarmSettingAction from "../../actions/AlarmSettingAction.jsx";
 
+
+const YEARSTEP = 4,
+      MONTHSTEP = 3,
+      DAYSTEP = 2;
+
 let AlarmSetting = React.createClass({
   mixins:[Navigation,State,React.addons.LinkedStateMixin],
   getInitialState: function() {
     return {
-      disable: true,
-      threshold: 100
+      disable: true
       };
   },
 
 
 	_onChange(){
     var alarmSettingData = AlarmSettingStore.getData();
-    this.setState({
-      threshold: alarmSettingData.AlarmThreshold
-    });
+    this.refs.threshold.getDOMNode().value = alarmSettingData.AlarmThreshold;
+    this.refs.openAlarm.setToggled(alarmSettingData.EnableStatus);
+    var stepValue = alarmSettingData.AlarmSteps;
+    for(var i = 0; i < stepValue.length; i++){
+      if(stepValue[i] === YEARSTEP){
+        this.refs.alarmSteps.refs.year.setChecked(true);
+      }
+      else if(stepValue[i] === MONTHSTEP){
+        this.refs.alarmSteps.refs.month.setChecked(true);
+      }
+      else if(stepValue[i] === DAYSTEP){
+        this.refs.alarmSteps.refs.day.setChecked(true);
+      }
+    }
 	},
 
-  loadDataByYear: function(year){
-
-  },
-
-
-  handleCheck: function(e) {
-    console.log(e.target.value);
-  },
 
   handleEdit: function() {
     this.setState({
@@ -54,22 +61,15 @@ let AlarmSetting = React.createClass({
     });
   },
 
-  handleChange: function(event) {
-    this.setState({
-      threshold: event.target.value
-    });
-  },
-
   render: function() {
     return (
       <div ref="alarmSettingDialog">
         <div className='jazz-setting-alarm-content'>
           <span>
-            <input className="jazz-setting-alarm-checkbox" type="checkbox" disabled={this.state.disable} />
-            <span>开启能耗报警</span>
+            <Toggle ref="openAlarm" label="开启能耗报警" labelPosition="right" disabled={this.state.disable}/>
           </span>
           <span className='jazz-setting-alarm-top'>
-            报警敏感度<input className='jazz-setting-alarm-input' type="text" value={this.state.threshold} disabled={this.state.disable} onChange={this.handleChange}/>%
+            报警敏感度<input ref="threshold" className='jazz-setting-alarm-input' type="text" disabled={this.state.disable}/>%
           </span>
           <span className='jazz-setting-alarm-top'>
             当数据高于基准值所设敏感度时，显示报警。
@@ -78,7 +78,7 @@ let AlarmSetting = React.createClass({
             对以下时段产生报警
           </span>
           <span className='jazz-setting-alarm-top'>
-            <Checkboxes handleCheck={this.handleCheck} disabled={this.state.disable} />
+            <Checkboxes ref="alarmSteps" disabled={this.state.disable}/>
           </span>
           <button type="submit" hidden={!this.state.disable} style={{width:'50px'}} onClick={this.handleEdit}> 编辑 </button>
           <span>
@@ -104,12 +104,9 @@ var Checkboxes = React.createClass({
 	render: function(){
 		return (
 			<span>
-				<input onClick={this.props.handleCheck}  name="stepCheckbox" type="checkbox" value="day"  disabled={this.props.disabled} />
-        日
-				<input onClick={this.props.handleCheck} name="stepCheckbox" type="checkbox" value="month"  disabled={this.props.disabled} />
-			  月
-				<input onClick={this.props.handleCheck} name="stepCheckbox" type="checkbox" value="year"  disabled={this.props.disabled} />
-        年
+				<Checkbox ref="day"  label="日" disabled={this.props.disabled}/>
+				<Checkbox ref="month"  label="月" disabled={this.props.disabled}/>
+				<Checkbox ref="year"  label="年" disabled={this.props.disabled}/>
 			</span>
     );
   }
