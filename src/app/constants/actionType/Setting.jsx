2cb2ae67f7a78c9f81630e4ce601003331d1a179
angelia-yuqi <yuqi-angelia.wang@schneider-electric.com>
2015-06-25 13:01:13 +0800

    baseline data get

--- src/app/constants/actionType/Setting.jsx
+++ src/app/constants/actionType/Setting.jsx
@@ -3,7 +3,10 @@ import keyMirror from 'keymirror';
 module.exports = {
   Action: keyMirror({
     LOAD_SETTING_DATA: null,
-    SAVE_SETTING_SUCESS: null,
-    SAVE_SETTING_ERROR: null
+    SAVE_SETTING_SUCCESS: null,
+    SAVE_SETTING_ERROR: null,
+    GET_MODIFY_DATA: null,
+    SET_MODIFY_DATA_SUCCESS: null,
+    SET_MODIFY_DATA_ERROR: null
   })
 };
---
+++ src/app/stores/BaselineModifyStore.jsx
@@ -0,0 +1,43 @@
+'use strict';
+
+import AppDispatcher from '../dispatcher/AppDispatcher.jsx';
+import PrototypeStore from './PrototypeStore.jsx';
+import assign from 'object-assign';
+import Immutable from 'immutable';
+import {Action} from '../constants/actionType/Setting.jsx';
+
+
+const SETTING_DATA_CHANGED_EVENT = 'modifydatachanged';
+var _baselineModifyData = {};
+
+var BaselineModifyStore = assign({},PrototypeStore,{
+  getData(){
+    return _baselineModifyData;
+  },
+  setData(data){
+    _baselineModifyData =  data;
+  },
+  addSettingDataListener: function(callback) {
+    this.on(SETTING_DATA_CHANGED_EVENT, callback);
+  },
+  emitSettingData: function() {
+    this.emit(SETTING_DATA_CHANGED_EVENT);
+  },
+  removeSettingDataListener: function(callback) {
+    this.removeListener(SETTING_DATA_CHANGED_EVENT, callback);
+  },
+});
+BaselineModifyStore.dispatchToken = AppDispatcher.register(function(action) {
+  switch(action.type) {
+    case Action.GET_MODIFY_DATA:
+      BaselineModifyStore.setData();
+      BaselineModifyStore.emitSettingData();
+      break;
+    case Action.SET_MODIFY_DATA_SUCCESS:
+      break;
+    case Action.SET_MODIFY_DATA_ERROR:
+      break;
+  }
+});
+
+module.exports = BaselineModifyStore;
