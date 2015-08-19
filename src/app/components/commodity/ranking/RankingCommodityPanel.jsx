c291d962e2151859b4bd42e340d3c267841a8e33
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2015-08-19 13:59:26 +0800

    update code for RankingCommodityPanel

--- src/app/components/commodity/ranking/RankingCommodityPanel.jsx
+++ src/app/components/commodity/ranking/RankingCommodityPanel.jsx
@@ -11,6 +11,7 @@ var RankingCommodityPanel = React.createClass({
 
   propTypes: {
     checkedTreeNodes: React.PropTypes.array,
+    ecType:React.PropTypes.string,
     //checkedCommodity:{commodityId:XX,commodityName:XX}
     checkedCommodity: React.PropTypes.object,
   },
@@ -38,7 +39,7 @@ var RankingCommodityPanel = React.createClass({
   },
   getInitialState:function(){
     return{
-      ecType:CommodityStore.getRankingECType(),
+      ecType:this.props.ecType,
       isLoading:false,
       isShow:false,
       commodityList:null,
