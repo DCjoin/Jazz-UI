66657742280b6be280238cdafd7c746adf5302e6
angelia-yuqi <yuqi-angelia.wang@schneider-electric.com>
2015-09-11 14:39:28 +0800

    error code

--- src/app/components/GlobalErrorMessageDialog.jsx
+++ src/app/components/GlobalErrorMessageDialog.jsx
@@ -1,35 +1,49 @@
 'use strict';
 import React from "react";
 
-import {Dialog, Snackbar, FlatButton} from 'material-ui';
+import { Dialog, Snackbar, FlatButton } from 'material-ui';
 
 let GlobalErrorMessageDialog = React.createClass({
-  getInitialState(){
+  getInitialState() {
     return {
-             isShowed: false,
-             errorMessage:''
-           };
+      isShowed: false,
+      errorMessage: '',
+      errorCode: ''
+    };
   },
-  _onDismiss(){
-    this.setState({isShowed: false});
+  _onDismiss() {
+    this.setState({
+      isShowed: false
+    });
   },
-  _show(){
+  _show() {
     this.refs.errorMessageDialog.show();
   },
-  _hide(){
+  _hide() {
     this.refs.errorMessageDialog.dismiss();
   },
-  render(){
-    let me = this;
-    var snackbar = <Snackbar
-      message={me.state.errorMessage}
-      onDismiss={me._onDismiss}
-      ref='errorMessageDialog' />;
+  render() {
+    var output = null;
+    var _buttonActions = [
+      <FlatButton label="确定" secondary={true} onClick={this._hide} />
+    ];
+    var snackbar = <Snackbar message={this.state.errorMessage} onDismiss={this._onDismiss} ref='errorMessageDialog' />;
+    var dialog = <Dialog title="Error Message" openImmediately={this.state.isShowed} actions={_buttonActions} modal={false} ref='errorMessageDialog' contentStyle={{
+      width: '500px',
+      color: '#464949'
+    }}>
+      <div> {this.state.errorMessage}</div>
+    </Dialog>;
+    if (this.state.errorCode == '1') {
+      output = dialog;
+    } else {
+      output = snackbar;
+    }
 
-    return  <div>{snackbar} </div>;
+    return <div>{output} </div>;
   },
-  componentDidUpdate(){
-    if(this.state.isShowed){
+  componentDidUpdate() {
+    if (this.state.isShowed) {
       this._show();
     }
   },
--- src/app/components/JazzApp.jsx
+++ src/app/components/JazzApp.jsx
@@ -6,129 +6,147 @@ import MainMenu from './MainMenu.jsx';
 import GlobalErrorMessageDialog from './GlobalErrorMessageDialog.jsx';
 import GlobalErrorMessageStore from '../stores/GlobalErrorMessageStore.jsx';
 
-import keyMirror from   'keymirror';
+import keyMirror from 'keymirror';
 
-let { Route, DefaultRoute, RouteHandler, Link, Navigation, State } = Router;
+let {Route, DefaultRoute, RouteHandler, Link, Navigation, State} = Router;
 
 window.REM = {};
-window.REM.popupNotes=function(msg){
+window.REM.popupNotes = function(msg) {
   window.alert(msg);
 };
 
 let JazzApp = React.createClass({
-    mixins:[Navigation,State],
-    childContextTypes:{
-        muiTheme: React.PropTypes.object.isRequired
-    },
-    getChildContext() {
-        return {
-            muiTheme: this.props.muiTheme
-        };
-    },
-    _showLoading:function(argument) {
-        this.setState({loading:true},function (argument) {
-            this.refs.ajax._show();
+  mixins: [Navigation, State],
+  childContextTypes: {
+    muiTheme: React.PropTypes.object.isRequired
+  },
+  getChildContext() {
+    return {
+      muiTheme: this.props.muiTheme
+    };
+  },
+  _showLoading: function(argument) {
+    this.setState({
+      loading: true
+    }, function(argument) {
+      this.refs.ajax._show();
+    });
+  },
+  _hideLoading: function(argument) {
+    this.refs.ajax._hide();
+  },
+  _showError: function(msg) {
+    this.setState({
+      loading: true
+    }, function(argument) {
+      this.refs.ajax._error(msg);
+    });
+  },
+  _setHighchartConfig() {
+    window.Highcharts.setOptions({
+      global: {
+        useUTC: false
+      },
+      lang: (function() {
+        var m = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+          w = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+          g = I18N.Common.Glossary,
+          mn = g.MonthName,
+          sn = g.ShortMonth,
+          wk = g.WeekDay,
+          ret = {
+            months: [],
+            shortMonths: [],
+            weekdays: []
+          };
+        m.forEach(function(n) {
+          ret.months.push(mn[n]);
+          ret.shortMonths.push(sn[n]);
         });
-    },
-    _hideLoading:function(argument) {
-        this.refs.ajax._hide();
-    },
-    _showError:function(msg) {
-        this.setState({loading:true},function (argument) {
-            this.refs.ajax._error(msg);
+        w.forEach(function(n) {
+          ret.weekdays.push(wk[n]);
         });
-    },
-    _setHighchartConfig(){
-      window.Highcharts.setOptions({
-          global: {
-              useUTC: false
-          },
-          lang: (function () {
-              var m = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-                  w = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-                  g = I18N.Common.Glossary,
-                  mn = g.MonthName,
-                  sn = g.ShortMonth,
-                  wk = g.WeekDay,
-                  ret = { months: [], shortMonths: [], weekdays: [] };
-              m.forEach(function (n) {
-                  ret.months.push(mn[n]);
-                  ret.shortMonths.push(sn[n]);
-              });
-              w.forEach(function (n) {
-                  ret.weekdays.push(wk[n]);
-              });
-              return ret;
-          })()
+        return ret;
+      })()
+    });
+  },
+  componentDidMount: function() {
+    var params = this.getParams();
+    var lang = params.lang;
+    var query = this.getQuery();
+    var routes = this.getRoutes();
+    var me = this;
+    var afterLoadLang = function(b) {
+      window.I18N = b;
+      var url = window.location.toLocaleString();
+      if (url.indexOf('menutype=energy') > -1) {
+        //  me.replaceWith('setting',{lang:lang});
+        me.replaceWith('map', {
+          lang: lang
+        });
+      } else {
+        //me.replaceWith('alarm',{lang:lang});
+        me.replaceWith('map', {
+          lang: lang
+        });
+      }
+      me._setHighchartConfig();
+    //me.transitionTo('main',{lang:lang});
+    };
+
+    if (!lang) {
+      lang = window.navigator.language.toLowerCase();
+      //this.setState({shouldRender : true});
+      this.replaceWith('app', {
+        lang: lang
       });
-    },
-    componentDidMount: function() {
-        var params = this.getParams();
-        var lang = params.lang;
-        var query = this.getQuery();
-        var routes = this.getRoutes();
-        var me = this;
-        var afterLoadLang = function(b) {
-            window.I18N=b;
-            var url = window.location.toLocaleString();
-            if(url.indexOf('menutype=energy')>-1){
-              //  me.replaceWith('setting',{lang:lang});
-                me.replaceWith('map',{lang:lang});
-            }else{
-                //me.replaceWith('alarm',{lang:lang});
-                me.replaceWith('map',{lang:lang});
-            }
-            me._setHighchartConfig();
-            //me.transitionTo('main',{lang:lang});
-        };
+    }
+    //afterLoadLang(I18N);
 
-        if(!lang){
-            lang = window.navigator.language.toLowerCase();
-            //this.setState({shouldRender : true});
-            this.replaceWith('app',{lang:lang});
-        }
-        //afterLoadLang(I18N);
+    //return;
 
-        //return;
+    if (lang.toLowerCase() == 'en-us') {
+      require(['../lang/zh-cn.js'], afterLoadLang); //should be changed when support english
+    } else {
+      require(['../lang/zh-cn.js'], afterLoadLang);
+    }
+    GlobalErrorMessageStore.addChangeListener(this._onErrorMessageChanged);
+  },
+  _onErrorMessageChanged() {
+    let errorMessage = GlobalErrorMessageStore.getErrorMessage();
+    let errorCode = GlobalErrorMessageStore.getErrorCode();
+    this.refs.globalErrorMessageDialog.setState({
+      isShowed: true,
+      errorMessage: errorMessage,
+      errorCode: errorCode
+    });
+  },
+  componentWillUnmount() {
+    GlobalErrorMessageStore.removeChangeListener(this._onErrorMessageChanged);
+  },
+  getInitialState: function() {
+    return {
+      shouldRender: false,
+      loading: false
+    };
+  },
+  render: function() {
+    // if(!this.state.shouldRender){
+    //     return (<div><RouteHandler {...this.props} ajax={this.state.ajaxCommon}  /></div>);
+    // }
+    var loading = null;
+    if (this.state.loading) {
+      loading = ''; //(<AjaxDialog ref="ajax" />);
+    }
 
-        if(lang.toLowerCase() == 'en-us'){
-            require(['../lang/zh-cn.js'],afterLoadLang);//should be changed when support english
-        }
-        else{
-            require(['../lang/zh-cn.js'],afterLoadLang);
-        }
-        GlobalErrorMessageStore.addChangeListener(this._onErrorMessageChanged);
-    },
-    _onErrorMessageChanged(){
-      let errorMessage = GlobalErrorMessageStore.getErrorMessage();
-      this.refs.globalErrorMessageDialog.setState({isShowed:true, errorMessage:errorMessage});
-    },
-    componentWillUnmount(){
-  	   GlobalErrorMessageStore.removeChangeListener(this._onErrorMessageChanged);
-  	},
-    getInitialState: function() {
-        return {
-            shouldRender:false,
-            loading:false
-        };
-    },
-    render: function () {
-        // if(!this.state.shouldRender){
-        //     return (<div><RouteHandler {...this.props} ajax={this.state.ajaxCommon}  /></div>);
-        // }
-        var loading = null;
-        if(this.state.loading){
-            loading = '';//(<AjaxDialog ref="ajax" />);
-        }
-
-        return (
-          <div className="jazz-app">
+    return (
+      <div className="jazz-app">
               <RouteHandler {...this.props} showLoading={this._showLoading} hideLoading={this._hideLoading} showError={this._showError} />
               {loading}
               <GlobalErrorMessageDialog ref='globalErrorMessageDialog'/>
           </div>
-        );
-    }
+      );
+  }
 });
 
 module.exports = JazzApp;
