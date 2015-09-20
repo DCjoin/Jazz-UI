380f2f7bcae99c21ab93d176ec8ad3458872ea2d
Alex <shaohong-alex.Feng@schneider-electric.com>
2015-09-20 11:41:14 +0800

    update code

--- src/app/components/energy/ChartStrategyFactor.jsx
+++ src/app/components/energy/ChartStrategyFactor.jsx
@@ -3999,8 +3999,25 @@ let ChartStrategyFactor = {
 
       let seriesNumber = EnergyStore.getEnergyData().get('Data').size;
       let charTypes = [];
+      let seriesStatusArray = ChartStatusStore.getSeriesStatus();
+      let sslength = seriesStatusArray.length;
       for (let i = 0; i < seriesNumber; i++) {
-        charTypes.push(chartType); //暂且全部用chartType，以后可以修改每个series type之后要做更改
+        let curChartType;
+        if (i < sslength) {
+          let serie = seriesStatusArray[i];
+          if (serie) {
+            if (serie.IsDisplay) {
+              curChartType = ChartStatusStore.getChartTypeByNum(serie.ChartType);
+            } else {
+              curChartType = null;
+            }
+          } else {
+            curChartType = chartType;
+          }
+        } else {
+          curChartType = chartType;
+        }
+        charTypes.push(curChartType);
       }
       params.charTypes = charTypes;
 
