6558b458f9aa64193b970f0492729e12115bfa9e
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2015-10-28 17:23:58 +0800

    fix bug 9049

--- src/app/components/commodity/CommonCommodityPanel.jsx
+++ src/app/components/commodity/CommonCommodityPanel.jsx
@@ -1,6 +1,6 @@
 'use strict';
 import React from "react";
-import {Navigation, State } from 'react-router';
+import { Navigation, State } from 'react-router';
 import CommodityStore from '../../stores/CommodityStore.jsx';
 import CommodityAction from '../../actions/CommodityAction.jsx';
 import CommonCommodityList from './CommonCommodityList.jsx';
@@ -8,128 +8,128 @@ import HierarchyButton from '../Hierarchy/HierarchyButton.jsx';
 import DimButton from '../Dim/DimButton.jsx';
 
 var CommonCommodityPanel = React.createClass({
-  mixins:[Navigation,State],
+  mixins: [Navigation, State],
   propTypes: {
-    ecType:React.PropTypes.string,
+    ecType: React.PropTypes.string,
   },
-  _onHierachyTreeClick:function(node){
-      if(node!=this.state.dimParentNode){
-        CommodityAction.setCurrentHierarchyInfo(node.Id,node.Name);
-        if(this.props.ecType=="Cost"){
-          this.refs.dimButton.resetButtonName();
-        }
-
-        this.setState({
-          dimActive:true,
-          dimParentNode:node,
-          HierarchyShow:false,
-          DimShow:true,
-          checkedCommodityList:null
-        });
+  _onHierachyTreeClick: function(node) {
+    if (node != this.state.dimParentNode) {
+      CommodityAction.setCurrentHierarchyInfo(node);
+      if (this.props.ecType == "Cost") {
+        this.refs.dimButton.resetButtonName();
       }
 
+      this.setState({
+        dimActive: true,
+        dimParentNode: node,
+        HierarchyShow: false,
+        DimShow: true,
+        checkedCommodityList: null
+      });
+    }
+
   },
-  _onHierarchButtonClick:function(){
+  _onHierarchButtonClick: function() {
     this.setState({
-      HierarchyShow:true,
-      DimShow:false
+      HierarchyShow: true,
+      DimShow: false
     });
   },
-  handleHierClickAway:function(){
+  handleHierClickAway: function() {
     this.setState({
-      HierarchyShow:false
+      HierarchyShow: false
     })
   },
-  _onDimTreeClick:function(node){
-    if(node.Id!==0){
+  _onDimTreeClick: function(node) {
+    if (node.Id !== 0) {
       CommodityAction.setCurrentDimInfo(node);
-    }
-    else {
+    } else {
       CommodityAction.setCurrentDimInfo(null);
     }
     this.setState({
-      HierarchyShow:true,
-      DimShow:false,
-      checkedCommodityList:null
+      HierarchyShow: true,
+      DimShow: false,
+      checkedCommodityList: null
     });
   },
-  _onDimButtonClick:function(){
+  _onDimButtonClick: function() {
     this.setState({
-      HierarchyShow:false,
-      DimShow:true
+      HierarchyShow: false,
+      DimShow: true
     })
   },
-  handleDimClickAway:function(){
+  handleDimClickAway: function() {
     this.setState({
-      DimShow:false
+      DimShow: false
     })
   },
-  getInitialState:function(){
-    return{
-      HierarchyShow:false,
-      dimActive:false,
-      dimParentNode:null,
-      DimShow:false,
-      checkedCommodityList:CommodityStore.getCommodityStatus(),
+  getInitialState: function() {
+    return {
+      HierarchyShow: false,
+      dimActive: false,
+      dimParentNode: null,
+      DimShow: false,
+      checkedCommodityList: CommodityStore.getCommodityStatus(),
     };
   },
 
   componentWillMount: function() {
-    let hierNode=CommodityStore.getHierNode();
-    if(!!hierNode){
-      let node={
-        Id:hierNode.hierId,
-        Name:hierNode.hierName
+    let hierNode = CommodityStore.getHierNode();
+    if (!!hierNode) {
+      let node = {
+        Id: hierNode.hierId,
+        Name: hierNode.hierName
       };
       this.setState({
-        dimParentNode:node,
-        dimActive:true,
+        dimParentNode: node,
+        dimActive: true,
       });
     }
   },
-  componentWillReceiveProps:function(){
+  componentWillReceiveProps: function() {
     this.setState({
-      checkedCommodityList:CommodityStore.getCommodityStatus()
+      checkedCommodityList: CommodityStore.getCommodityStatus()
     });
   },
-  render:function(){
-    let CurrentHierId=CommodityStore.getCurrentHierarchyId(),
-        CurrentDimId=!!CommodityStore.getCurrentDimNode()?CommodityStore.getCurrentDimNode().dimId:null;
+  render: function() {
+    let CurrentHierId = CommodityStore.getCurrentHierarchyId(),
+      CurrentDimId = !!CommodityStore.getCurrentDimNode() ? CommodityStore.getCurrentDimNode().dimId : null;
 
-    let header,content;
+    let header, content;
     //header
-    if(this.props.ecType=="Carbon"){
-      header=(
+    if (this.props.ecType == "Carbon") {
+      header = (
         <div className="header">
           <HierarchyButton hierId={CurrentHierId}
-                            onTreeClick={this._onHierachyTreeClick}
-                            onButtonClick={this._onHierarchButtonClick}
-                            show={this.state.HierarchyShow}
-                            handleClickAway={this.handleHierClickAway}/>
+        onTreeClick={this._onHierachyTreeClick}
+        onButtonClick={this._onHierarchButtonClick}
+        show={this.state.HierarchyShow}
+        handleClickAway={this.handleHierClickAway}/>
         </div>
 
 
-                            )
-    }
-    else{
-      if(this.props.ecType=="Cost"){
-        header=(
+      )
+    } else {
+      if (this.props.ecType == "Cost") {
+        header = (
           <div className="header">
             <HierarchyButton hierId={CurrentHierId}
-                              onTreeClick={this._onHierachyTreeClick}
-                              onButtonClick={this._onHierarchButtonClick}
-                              show={this.state.HierarchyShow}
-                              handleClickAway={this.handleHierClickAway}/>
+          onTreeClick={this._onHierachyTreeClick}
+          onButtonClick={this._onHierarchButtonClick}
+          show={this.state.HierarchyShow}
+          handleClickAway={this.handleHierClickAway}/>
 
-            <div style={{color:'#ffffff'}}>-</div>
+            <div style={{
+            color: '#ffffff'
+          }}>-</div>
 
             <DimButton ref={'dimButton'}
-                       active={this.state.dimActive}
-                       onTreeClick={this._onDimTreeClick}
-                       parentNode={this.state.dimParentNode}
-                       onButtonClick={this._onDimButtonClick}
-                       show={this.state.DimShow}
-                       handleClickAway={this.handleDimClickAway}/>
+          active={this.state.dimActive}
+          onTreeClick={this._onDimTreeClick}
+          parentNode={this.state.dimParentNode}
+          onButtonClick={this._onDimButtonClick}
+          show={this.state.DimShow}
+          handleClickAway={this.handleDimClickAway}/>
           </div>
 
         )
@@ -139,15 +139,17 @@ var CommonCommodityPanel = React.createClass({
       }
     }
     //content
-    if(CurrentHierId!==null){
-      content=<div style={{display:'flex'}}><CommonCommodityList checkedCommodityList={this.state.checkedCommodityList}/></div>
+    if (CurrentHierId !== null) {
+      content = <div style={{
+        display: 'flex'
+      }}><CommonCommodityList checkedCommodityList={this.state.checkedCommodityList}/></div>
     }
-    return(
+    return (
       <div className="jazz-dataselectmainpanel">
           {header}
           {content}
       </div>
-    )
+      )
   }
 });
 
