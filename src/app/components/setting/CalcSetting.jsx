893d4c8c5b5f814f3d288a757ae2b69fdeb1dd4f
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2015-10-29 14:33:52 +0800

    fix bug 8222

--- src/app/components/setting/CalcSetting.jsx
+++ src/app/components/setting/CalcSetting.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, DefaultRoute, RouteHandler, Link, Navigation, State } from 'react-router';
-import {SvgIcon, IconButton, DropDownMenu, TextField, FlatButton, FloatingActionButton, RadioButtonGroup, RadioButton, DatePicker,RaisedButton,CircularProgress } from 'material-ui';
+import { SvgIcon, IconButton, DropDownMenu, TextField, FlatButton, FloatingActionButton, RadioButtonGroup, RadioButton, DatePicker, RaisedButton, CircularProgress } from 'material-ui';
 import assign from "object-assign";
 import classNames from 'classnames';
 import YearPicker from '../../controls/YearPicker.jsx';
@@ -22,16 +22,18 @@ var CalcSetting = React.createClass({
     onCalc: React.PropTypes.func,
     isViewStatus: React.PropTypes.bool,
     isDisplay: React.PropTypes.bool,
-    dateRange:  React.PropTypes.object,
+    dateRange: React.PropTypes.object,
   },
 
-  getDefaultProps:function(){
-    return {items: []};
+  getDefaultProps: function() {
+    return {
+      items: []
+    };
   },
 
-  getValue: function(){
+  getValue: function() {
     var arr = [];
-    if(!this.refs['item1']){
+    if (!this.refs['item1']) {
       return arr;
     }
     for (var i = 1; i < 25; i++) {
@@ -42,7 +44,7 @@ var CalcSetting = React.createClass({
     return arr;
   },
 
-  validate: function(){
+  validate: function() {
     var startDate = new Date(this.props.dateRange.start),
       endDate = new Date(this.props.dateRange.end),
       tmpDate = new Date(startDate);
@@ -50,60 +52,67 @@ var CalcSetting = React.createClass({
     return tmpDate >= endDate;
   },
 
-  _onCalcClick: function(){
-    if(this.props.onCalc){
+  _onCalcClick: function() {
+    if (this.props.onCalc) {
       this.props.onCalc();
     }
   },
 
-  render: function () {
-    if(!this.props.isDisplay){
+  render: function() {
+    if (!this.props.isDisplay) {
       return <div></div>;
     }
-    if(!this.validate()){
-      return <div style={{color:'red', fontSize: 12}}>所选数据的时间跨度大于一个月，无法计算，请重新选择数据</div>;
+    if (!this.validate() && !this.props.isViewStatus) {
+      return <div style={{
+          color: 'red',
+          fontSize: 12
+        }}>所选数据的时间跨度大于一个月，无法计算，请重新选择数据</div>;
     }
-    var items = this.props.items || [], rows = [];
-    var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+    var items = this.props.items || [],
+      rows = [];
+    var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
     var me = this;
-    var createItem = function(item, index){
-      for (var i = 0; i < items.length; i++) {
-        if(items[i].TBTime == item){
-          var props = {
-            tag: me.props.tag,
-            ref: 'item'+ item,
-            time: item,
-            val1: items[i].WorkDayValue,
-            val2: items[i].HolidayDayValue,
-            val1Mod: items[i].WorkDayValueStatus,
-            val2Mod: items[i].HolidayValueStatus,
-            isViewStatus: me.props.isViewStatus,
-          };
-          return <CalcItem {...props} />;
+    var createItem = function(item, index) {
+        for (var i = 0; i < items.length; i++) {
+          if (items[i].TBTime == item) {
+            var props = {
+              tag: me.props.tag,
+              ref: 'item' + item,
+              time: item,
+              val1: items[i].WorkDayValue,
+              val2: items[i].HolidayDayValue,
+              val1Mod: items[i].WorkDayValueStatus,
+              val2Mod: items[i].HolidayValueStatus,
+              isViewStatus: me.props.isViewStatus,
+            };
+            return <CalcItem {...props} />;
+          }
         }
-      }
-      var props = {
-        tag: me.props.tag,
-        ref: 'item'+ item,
-        time: item,
-        isViewStatus: me.props.isViewStatus,
-        val1: '',
-        val2: '',
-        val1Mod: false,
-        val2Mod: false,
-      };
-      return <CalcItem {...props} />;
-    },
-    rows = arr.map(createItem);
+        var props = {
+          tag: me.props.tag,
+          ref: 'item' + item,
+          time: item,
+          isViewStatus: me.props.isViewStatus,
+          val1: '',
+          val2: '',
+          val1Mod: false,
+          val2Mod: false,
+        };
+        return <CalcItem {...props} />;
+      },
+      rows = arr.map(createItem);
     var style = {
       margin: "18px 0",
-      padding:'9px',
-      border:"1px solid #efefef",
+      padding: '9px',
+      border: "1px solid #efefef",
     };
 
     var reCalcCtrl;
-    if(!this.props.isViewStatus){
-      reCalcCtrl = <a href="javascript:void(0)" onClick={this._onCalcClick}  style={{color:'#1ca8dd','margin-left':'27px'}}>重新计算</a>;
+    if (!this.props.isViewStatus) {
+      reCalcCtrl = <a href="javascript:void(0)" onClick={this._onCalcClick}  style={{
+        color: '#1ca8dd',
+        'margin-left': '27px'
+      }}>重新计算</a>;
     }
 
     return (
@@ -120,7 +129,7 @@ var CalcSetting = React.createClass({
         </div>
         {reCalcCtrl}
       </div>
-    );
+      );
   }
 });
 
