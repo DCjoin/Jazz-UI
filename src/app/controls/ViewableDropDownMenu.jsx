96e8f2f1aad458d8ae26e62d74ccabadcf2354ed
angelia-yuqi <yuqi-angelia.wang@schneider-electric.com>
2015-12-15 11:05:25 +0800

    dropDownMenu fix bug

--- src/app/controls/ViewableDropDownMenu.jsx
+++ src/app/controls/ViewableDropDownMenu.jsx
@@ -85,6 +85,9 @@ var ViewableDropDownMenu = React.createClass({
   render: function() {
     var dropDownMenu;
     var text = this.props.textField;
+    if (this.props.dataItems === undefined) {
+      return null;
+    }
     var menuItems = this.props.dataItems.map((item, id) => {
       return {
         payload: item.payload,
@@ -136,7 +139,11 @@ var ViewableDropDownMenu = React.createClass({
             return true;
           }
         });
-        value = this.props.dataItems[index][text];
+        if (index !== -1) {
+          value = this.props.dataItems[index][text];
+        } else {
+          value = null;
+        }
       } else if (this.props.dataItems.length > 0) {
         value = this.props.dataItems[0][text];
       }
