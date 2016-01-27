e518d2bbd379f06f1dbde61f9553bafa24702dcd
angelia-yuqi <yuqi-angelia.wang@schneider-electric.com>
2016-01-27 13:23:51 +0800

    error handle

--- src/app/components/GlobalErrorMessageDialog.jsx
+++ src/app/components/GlobalErrorMessageDialog.jsx
@@ -6,6 +6,7 @@ import { Dialog, Snackbar, FlatButton } from 'material-ui';
 let GlobalErrorMessageDialog = React.createClass({
   getInitialState() {
     return {
+      isShowed: false,
       errorMessage: '',
       errorCode: '',
       dialogShow: false
@@ -13,9 +14,7 @@ let GlobalErrorMessageDialog = React.createClass({
   },
   _onDismiss() {
     this.setState({
-      errorMessage: '',
-      errorCode: '',
-      dialogShow: false
+      isShowed: false
     });
   },
   render() {
@@ -26,21 +25,20 @@ let GlobalErrorMessageDialog = React.createClass({
     ];
     var snackbar = <Snackbar style={{
       maxWidth: 'none'
-    }} message={this.state.errorMessage} onDismiss={this._onDismiss} ref='errorMessageDialog' />;
+    }} message={this.state.errorMessage} openOnMount={true} onDismiss={this._onDismiss} ref='errorMessageDialog' />;
     var dialog = <Dialog title="" openImmediately={true} actions={_buttonActions} modal={false} ref='errorMessageDialog' contentStyle={{
       width: '500px',
       color: '#464949'
     }}>
       <div> {this.state.errorMessage}</div>
     </Dialog>;
-    if (this.state.errorCode !== '') {
+    if (this.state.isShowed) {
       if (errorCodeArr.indexOf(this.state.errorCode) !== -1 || this.state.dialogShow) {
         output = dialog;
       } else {
         output = snackbar;
       }
     }
-
     return <div>{output} </div>;
   }
 });
