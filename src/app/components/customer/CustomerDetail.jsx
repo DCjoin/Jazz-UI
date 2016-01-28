8e7a69b82628718575301b775d6401cb9433b4c5
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2016-01-28 10:47:44 +0800

    update

--- src/app/components/customer/CustomerDetail.jsx
+++ src/app/components/customer/CustomerDetail.jsx
@@ -291,7 +291,7 @@ var CustomerDetail = React.createClass({
           <div className="pop-user-detail-content-item">
             <Checkbox {...calStatusProps} />
           </div>
-          <div className="pop-user-detail-content-item">
+          <div className={classnames("pop-user-detail-content-item", "jazz-customer-comment")}>
             <ViewableTextField {...userCommentProps}/>
           </div>
         </div>
