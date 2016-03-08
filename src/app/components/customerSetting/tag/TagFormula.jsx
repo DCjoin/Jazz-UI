690c07adc0fd396f621797386f488afc395b7333
angelia-yuqi <yuqi-angelia.wang@schneider-electric.com>
2016-03-08 09:45:36 +0800

    upload code

--- src/app/components/customerSetting/tag/TagFormula.jsx
+++ src/app/components/customerSetting/tag/TagFormula.jsx
@@ -1,8 +1,6 @@
 'use strict';
 
 import React from "react";
-import { Checkbox } from 'material-ui';
-import Regex from '../../../constants/Regex.jsx';
 import ViewableTextField from '../../../controls/ViewableTextField.jsx';
 import MonitorTag from './MonitorTag.jsx';
 
@@ -16,25 +14,6 @@ var TagFormula = React.createClass({
     return {
     };
   },
-  validateFormula: function(exp) {
-    var regStr = /(-\d+)(\.\d+)?/g; //匹配负浮点数
-    var regStr1 = /\{[\u4e00-\u9fa50-9a-zA-Z_\(\)\-\[\]\#\&\,\:\;\.\~\+\%\\\/\|]*\}/g;
-    var regStr2 = /-\{[\u4e00-\u9fa50-9a-zA-Z_\(\)\-\[\]\#\&\,\:\;\.\~\+\%\\\/\|]*\}/g;
-    //var regStr3 = /^[\+|-|\*|\/]{1}\{[A-z]+[A-z0-9]*\}$/; //匹配‘{}’在公式结尾
-
-    exp = exp.replace(/\s+/g, ''); //替换空白字符
-
-    exp = exp.replace(regStr2, '+(8)'); //-tag替换成正常量
-    exp = exp.replace(regStr1, '(7)'); //tag替换成正常量
-    exp = exp.replace(regStr, '+(9)'); //负常量替换成正常量
-
-    var reg = /[\d\.\+\-\*\/\(\)]+/;
-    try {
-      return reg.test(exp) && !isNaN(eval("(" + exp + ")")) ? "" : I18N.Setting.Tag.InvalidFormula;
-    } catch (e) {
-      return I18N.Setting.Tag.InvalidFormula;
-    }
-  },
   _onRowClick: function(type, code) {
     var value = this.refs.formula.getValue();
     var tagType;
@@ -47,6 +26,11 @@ var TagFormula = React.createClass({
     this.refs.formula.setState({
       value: newValue,
       errorText: this.refs.formula._getError(newValue)
+    }, () => {
+      this.props.mergeTag({
+        value: newValue,
+        path: "Formula"
+      });
     });
   },
   _renderFormula: function() {
@@ -83,6 +67,25 @@ var TagFormula = React.createClass({
       table = <MonitorTag tagId={this.props.selectedTag.get('Id')} onRowClick={this._onRowClick}/>;
     }
     return table;
+  },
+  validateFormula: function(exp) {
+    var regStr = /(-\d+)(\.\d+)?/g; //匹配负浮点数
+    var regStr1 = /\{[\u4e00-\u9fa50-9a-zA-Z_\(\)\-\[\]\#\&\,\:\;\.\~\+\%\\\/\|]*\}/g;
+    var regStr2 = /-\{[\u4e00-\u9fa50-9a-zA-Z_\(\)\-\[\]\#\&\,\:\;\.\~\+\%\\\/\|]*\}/g;
+    //var regStr3 = /^[\+|-|\*|\/]{1}\{[A-z]+[A-z0-9]*\}$/; //匹配‘{}’在公式结尾
+
+    exp = exp.replace(/\s+/g, ''); //替换空白字符
+
+    exp = exp.replace(regStr2, '+(8)'); //-tag替换成正常量
+    exp = exp.replace(regStr1, '(7)'); //tag替换成正常量
+    exp = exp.replace(regStr, '+(9)'); //负常量替换成正常量
+
+    var reg = /[\d\.\+\-\*\/\(\)]+/;
+    try {
+      return reg.test(exp) && !isNaN(eval("(" + exp + ")")) ? "" : I18N.Setting.Tag.InvalidFormula;
+    } catch (e) {
+      return I18N.Setting.Tag.InvalidFormula;
+    }
   },
 
   componentWillMount: function() {},
