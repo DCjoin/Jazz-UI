b4935ce89fbb1ee59fd20fffc1834cb5727d5cb1
vonqi@qq.com <vonqi@qq.com>
2016-04-29 11:10:47 +0800

    解决webpackage路径配置错误

--- make-webpack-config.js
+++ make-webpack-config.js
@@ -26,7 +26,7 @@ module.exports = function(options) {
 
   var publicPath = options.devServer ?
     "http://localhost:3000/build/" :
-    "/assets/"; //"./";
+    "/webapihost/assets/"; //"./";
   var output = {
     path: path.join(__dirname, "build", "assets"),
     publicPath: publicPath,
