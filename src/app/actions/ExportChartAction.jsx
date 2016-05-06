9b02d66ba7ec4f94598ebadff3671cd813b15544
angelia-yuqi <yuqi-angelia.wang@schneider-electric.com>
2016-05-06 13:34:09 +0800

    export bug

--- src/app/actions/ExportChartAction.jsx
+++ src/app/actions/ExportChartAction.jsx
@@ -1,6 +1,6 @@
 'use strict';
 import AppDispatcher from '../dispatcher/AppDispatcher.jsx';
-import ActionTypes from '../constants/actionType/Energy.jsx';
+import { Action } from '../constants/actionType/Energy.jsx';
 //import Path from '../constants/Path.jsx';
 import Config from 'config';
 
@@ -9,7 +9,7 @@ let ExportChartAction = {
   getTagsData4Export(params, path) {
     let fullPath = Config.ServeAddress + Config.APIBasePath + path;
     AppDispatcher.dispatch({
-      type: ActionTypes.EXPORT_CHART_ACTION_TYPE,
+      type: Action.EXPORT_CHART_ACTION_TYPE,
       params: params,
       path: fullPath
     });
