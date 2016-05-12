a2899a05d1e16061308b2b33ab0d6afa9bacd514
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2016-05-12 16:07:26 +0800

    update code

--- src/app/components/ContactUS.jsx
+++ src/app/components/ContactUS.jsx
@@ -1,24 +1,15 @@
 'use strict';
 
 import React from 'react';
-import get from 'lodash/object/get';
-import { CircularProgress, Snackbar } from 'material-ui';
-import LoginActionCreator from '../actions/LoginActionCreator.jsx';
-import LoginStore from '../stores/LoginStore.jsx';
-import LinkButton from '../controls/LinkButton.jsx';
-import Regex from '../constants/Regex.jsx';
-import CusFlatButton from '../controls/FlatButton.jsx';
-import { FlatButton } from 'material-ui';
-import Dialog from '../controls/PopupDialog.jsx';
-import ViewableTextField from '../controls/ViewableTextField.jsx';
+
 
 var ContactUS = React.createClass({
-	contextTypes: {
-		router: React.PropTypes.func
-	},
+  contextTypes: {
+    router: React.PropTypes.func
+  },
 
-	render() {
-		console.log('this.props.params.lang:'+this.props.params.lang);
+  render() {
+    console.log('this.props.params.lang:' + this.props.params.lang);
     return (
       <div className="jazz-contactus">
 				<div className="jazz-contactus-header">
@@ -31,7 +22,9 @@ var ContactUS = React.createClass({
 						<span>{I18N.ContactUS.Tips}</span>
 						<h3>{I18N.ContactUS.Business}</h3>
 						<div className="business">
-							<div className="card" style={{marginRight:'30px'}}>
+							<div className="card" style={{
+        marginRight: '30px'
+      }}>
 								<div><span className="cardName">张磊</span><span>{I18N.ContactUS.BusinessManager}</span></div>
 								<div><span>186 0085 5282</span></div>
 								<div><a href="mailto:Lei-Johnny.Zhang@schneider-electric.com">Lei-Johnny.Zhang@schneider-electric.com</a></div>
@@ -44,7 +37,9 @@ var ContactUS = React.createClass({
 						</div>
 						<h3>{I18N.ContactUS.Technology}</h3>
 						<div className="technology">
-							<div className="card" style={{marginRight:'30px'}}>
+							<div className="card" style={{
+        marginRight: '30px'
+      }}>
 								<div><span className="cardName">云晓旭</span><span>{I18N.ContactUS.TechnicalSupport}</span></div>
 								<div><span>186 1175 7398</span></div>
 								<div><a href="mailto:xiaoxu-bean.yun@schneider-electric.com">xiaoxu-bean.yun@schneider-electric.com</a></div>
@@ -59,7 +54,9 @@ var ContactUS = React.createClass({
 
 
 					<div className="jazz-contactus-content-QRcode">
-						<h4 style={{width:'200px'}}>{I18N.ContactUS.MicroSite}</h4>
+						<h4 style={{
+        width: '200px'
+      }}>{I18N.ContactUS.MicroSite}</h4>
 						<h5>{I18N.ContactUS.Scan}</h5>
 		        <div className="QRcode"></div>
 					</div>
@@ -68,8 +65,8 @@ var ContactUS = React.createClass({
 
 
       </div>
-    );
-	}
+      );
+  }
 });
 
 module.exports = ContactUS;
