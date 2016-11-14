dbc3e91818568fd4fac4c1ae1c49e2bc318b7d86
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2016-11-14 14:33:28 +0800

    fix bug #15515

--- src/app/config/local.jsx
+++ src/app/config/local.jsx
@@ -2,7 +2,8 @@ export default {
   //ServeAddress: "http://127.0.0.1:80/webapihost",
   //ServeAddress: "http://sechina.prod26.energymost.com/webapihost",
   //ServeAddress: "http://Rock1.test26.energymost.com/webapihost",
-  ServeAddress: "http://sp1.test26.energymost.com/webapihost",
+  //ServeAddress: "http://sp1.test26.energymost.com/webapihost",
+  ServeAddress: "http://sp1.test28.energymost.com/webapihost",
   APIBasePath: "/API",
   socketServer: "",
 }
