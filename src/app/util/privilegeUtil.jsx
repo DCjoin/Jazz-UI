1820b4d88c8395fa0736fcd3749631852f536077
Lawrence <liang-lawrence.liu@schneider-electric.com>
2016-11-22 15:49:35 +0800

    change role page

---
+++ src/app/util/privilegeUtil.jsx
@@ -0,0 +1,51 @@
+import PermissionCode, {
+	MAP_VIEW, ENERGY_MANAGE,
+	BASELINE_CONFIG, ENERGY_EXPORT, ENERGY_ALARM, DATA_REPORT_MANAGEMENT,
+	TAG_MANAGEMENT, HIERARCHY_MANAGEMENT, CUSTOM_LABELING, PLATFORM_MANAGEMENT
+} from 'constants/PermissionCode.jsx';
+
+import {remove} from 'lodash/array';
+
+const PrivilegeUtil = {
+	getCommonPrivilegeList: () => {
+		return [MAP_VIEW, ENERGY_MANAGE];
+	},
+	getRolePrivilegeList: () => {
+		return [
+			BASELINE_CONFIG, 
+			ENERGY_EXPORT, 
+			ENERGY_ALARM, 
+			DATA_REPORT_MANAGEMENT,
+			TAG_MANAGEMENT, 
+			HIERARCHY_MANAGEMENT, 
+			CUSTOM_LABELING, 
+			PLATFORM_MANAGEMENT
+		];
+	},
+
+	findView: (targetCodeObj, privilegeCodes) => privilegeCodes.indexOf(targetCodeObj.READONLY + ''),
+	findFull: (targetCodeObj, privilegeCodes) => privilegeCodes.indexOf(targetCodeObj.FULL + ''),
+
+	isView: (...arg) => PrivilegeUtil.findView(...arg) !== -1,
+
+	isFull: (...arg) => PrivilegeUtil.findFull(...arg) !== -1,
+
+	canView: (...arg) => PrivilegeUtil.isView(...arg)
+					||	 PrivilegeUtil.isFull(...arg),
+
+	getViewCode: (targetCodeObj, privilegeCodes) => PrivilegeUtil.isView(targetCodeObj, privilegeCodes) && targetCodeObj.READONLY,
+	getFullCode: (targetCodeObj, privilegeCodes) => PrivilegeUtil.isFull(targetCodeObj, privilegeCodes) && targetCodeObj.FULL,
+
+	changePrivilegeCodes: (targetCodeObj, privilegeCodes, value) => {
+		remove(privilegeCodes, code => code === targetCodeObj.FULL + "" );
+		remove(privilegeCodes, code => code === targetCodeObj.READONLY + "" );
+
+		if(value) {
+			privilegeCodes.push(value + "");
+		}
+
+		return privilegeCodes;
+	},
+};
+
+module.exports = PrivilegeUtil;
--- src/server/server.js
+++ src/server/server.js
@@ -81,7 +81,7 @@ server.route({
   handler: {
     proxy: {
     	passThrough: true,
-        uri: 'http://sp1.test28.energymost.com/webapihost/{path}',
+        uri: 'http://sp1.test26.energymost.com/webapihost/{path}',
         onResponse: function (err, res, request, reply, settings, ttl) {
             return reply(res);
         }
