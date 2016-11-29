8af341725f41c176607d832da9a66b0d640f64ef
Lawrence <liang-lawrence.liu@schneider-electric.com>
2016-11-29 15:12:05 +0800

    fix bug

--- src/app/dispatcher/AppDispatcher.jsx
+++ src/app/dispatcher/AppDispatcher.jsx
@@ -7,20 +7,15 @@ let queuePayload = [];
 let AppDispatcher = new Dispatcher();
 
 let oldDispatch = AppDispatcher.dispatch;
-let oldStopDispatching = AppDispatcher._stopDispatching;
 
 AppDispatcher.dispatch = function(payload) {
 	if(AppDispatcher.isDispatching()) {
-		queuePayload.push(payload);
+		setTimeout( function() {
+			AppDispatcher.dispatch(payload);
+		}, 0);
 	} else {
 		oldDispatch.call(this, payload);
 	}
-};
-AppDispatcher._stopDispatching = function(payload) {
-	for(let i = 0; i < queuePayload.length; i++) {
-		AppDispatcher.dispatch(queuePayload[i]);
-	}
-	queuePayload = [];	
 };
 
 module.exports = AppDispatcher;
