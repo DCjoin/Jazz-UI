b80349db576f6e00c7baf5e72036f7d0dc4d69cd
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2017-01-09 09:50:46 +0800

    update for report

--- src/app/actions/KPI/ReportAction.jsx
+++ src/app/actions/KPI/ReportAction.jsx
@@ -6,6 +6,28 @@ import util from 'util/Util.jsx';
 
 
 const ReportAction = {
+  getTemplateListByCustomerId(customerId, sortBy, order) {
+    Ajax.post('/DataReport/GetExportTemplateByCustomerId', {
+      params: {
+        dto: {
+          customerId: customerId,
+          sortBy: sortBy,
+          order: order
+        }
+      },
+      success: function(templateList) {
+        AppDispatcher.dispatch({
+          type: Action.GET_REPORT_TEMPLATE_LIST_SUCCESS,
+          templateList: templateList
+        });
+      },
+      error: function(err, res) {
+        AppDispatcher.dispatch({
+          type: Action.GET_REPORT_TEMPLATE_LIST_ERROR
+        });
+      }
+    });
+  },
 }
 
 export default ReportAction;
--- src/app/stores/KPI/ReportStore.jsx
+++ src/app/stores/KPI/ReportStore.jsx
@@ -10,16 +10,46 @@ import {List} from 'immutable';
 import _ from 'lodash';
 import CommonFuns from 'util/Util.jsx';
 
+let _templateList=null;
 const ReportStore = assign({}, PrototypeStore, {
-
+  getTemplateList() {
+    return _templateList;
+  },
+  setTemplateList(templateList) {
+      _templateList = Immutable.fromJS(templateList);
+  },
+  getTemplateItems: function(templateList) {
+    var Items=[];
+  if (templateList && templateList.size !== 0) {
+    Items=templateList.map(function(item) {
+      return {
+        payload: item.get('Id'),
+        text: item.get('Name')
+      };
+    }).toJS();
+  }
+  Items.unshift({
+    payload: -1,
+    text:I18N.EM.Report.Select
+  });
+  return Items
+  },
   dispose(){
-
+    _templateList=null;
   }
 
 });
 
 ReportStore.dispatchToken = AppDispatcher.register(function(action) {
   switch (action.type) {
+    case Action.GET_REPORT_TEMPLATE_LIST_SUCCESS:
+      ReportStore.setTemplateList(action.templateList);
+      ReportStore.emitChange();
+      break;
+    case Action.GET_REPORT_TEMPLATE_LIST_ERROR:
+      ReportStore.setTemplateList([]);
+      ReportStore.emitChange();
+      break;
     default:
   }
 });
