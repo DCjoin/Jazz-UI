231bf63adaf2cf048c9f6dbac820cbe9811fb528
Lawrence <liang-lawrence.liu@schneider-electric.com>
2017-01-10 11:20:55 +0800

    change report ui

--- src/app/components/KPI/ReportChart.jsx
+++ src/app/components/KPI/ReportChart.jsx
@@ -9,11 +9,13 @@ export default class ReportChart extends Component {
 					<div>{'瑞吉与物业越用电量结算'}</div>
 					<div>{'空调机房用电统计'}</div>
 				</div>
-				<iframe 
-					src="https://view.officeapps.live.com/op/view.aspx?src=http%3A%2F%2Fsejazz-test.oss-cn-hangzhou.aliyuncs.com%2FChina_Conclusions.xlsx" 
-					border="0" 
-					height='600'
-					width='100%'/>
+				<div className='jazz-report-chart-table-wrapper'>
+					<iframe 
+						src="https://view.officeapps.live.com/op/view.aspx?src=http%3A%2F%2Fsejazz-test.oss-cn-hangzhou.aliyuncs.com%2FChina_Conclusions.xlsx" 
+						border="0" 
+						height='600'
+						width='100%'/>
+				</div>
 			</div>
 		);
 	}
