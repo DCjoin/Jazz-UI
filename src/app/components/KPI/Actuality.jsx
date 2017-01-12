0a32aa8bbb33e99d4fed0c9c8ea937a791a8b74f
Lawrence <liang-lawrence.liu@schneider-electric.com>
2017-01-12 10:16:45 +0800

    fix bug with select building

--- src/app/components/KPI/Actuality.jsx
+++ src/app/components/KPI/Actuality.jsx
@@ -106,7 +106,7 @@ export default class Actuality extends Component {
 	}
 	_getSelectedHierarchy() {
 		let selectedHierarchyId = this._getHierarchyId(this.props);
-		return find(HierarchyStore.getBuildingList().concat(getCustomerById(this.props.router.params.customerId)), building => building.Id === selectedHierarchyId);
+		return find(HierarchyStore.getBuildingList().concat(getCustomerById(this.props.router.params.customerId)), building => building.Id === selectedHierarchyId) || null;
 	}
 	_routerPush(path) {
 		this.props.router.push(path);
