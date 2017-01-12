773265bfcafa2920db0f18a0f9a42554474ee8f2
Lawrence <liang-lawrence.liu@schneider-electric.com>
2017-01-12 17:26:02 +0800

    add permission

--- src/app/components/KPI/ReportChart.jsx
+++ src/app/components/KPI/ReportChart.jsx
@@ -3,6 +3,13 @@ import CircularProgress from 'material-ui/CircularProgress';
 
 import LinkButton from 'controls/LinkButton.jsx';
 
+import CurrentUserStore from 'stores/CurrentUserStore.jsx';
+import privilegeUtil from 'util/privilegeUtil.jsx';
+import PermissionCode from 'constants/PermissionCode.jsx';
+
+function isFull() {
+	return privilegeUtil.isFull(PermissionCode.INDEX_AND_REPORT, CurrentUserStore.getCurrentPrivilege());
+}
 export default class ReportChart extends Component {
 	render() {
 		let {data, url, onEdit, onDelete, onSetFirst, onDownload} = this.props,
@@ -10,12 +17,13 @@ export default class ReportChart extends Component {
 		if( !url ) {
 			return (<div className='jazz-report-chart-table-wrapper flex-center' style={{height: 600}}><CircularProgress mode="indeterminate" size={80} /></div>);
 		}
+
 		return (
 			<div className='jazz-report-chart-table-wrapper'>
 				<div className='jazz-report-chart-table-header'>
 					<ul className='jazz-report-chart-table-header-action'>
-						<li><LinkButton onClick={() => onEdit(id)} label={'设置'}/></li>|
-						<li><LinkButton onClick={() => onDelete(id)} label={'删除'}/></li>|
+						{isFull() && <li><LinkButton onClick={() => onEdit(id)} label={'设置'}/></li>}{isFull() && '|'}
+						{isFull() && <li><LinkButton onClick={() => onDelete(id)} label={'删除'}/></li>}{isFull() && '|'}
 						<li><LinkButton onClick={() => onSetFirst(id)} label={'设置为首个报表'}/></li>
 					</ul>
 					<div>{data.get('Name')}</div>
