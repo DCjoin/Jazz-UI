2b4683bf3f5fe32f80b2ef716a5b9c3b1cb3acf8
Lawrence <liang-lawrence.liu@schneider-electric.com>
2017-01-18 14:48:25 +0800

    fix bug #16252

--- src/app/components/KPI/ReportChart.jsx
+++ src/app/components/KPI/ReportChart.jsx
@@ -17,26 +17,29 @@ export default class ReportChart extends Component {
 		if( url === null ) {
 			return (<div className='jazz-report-chart-table-wrapper flex-center' style={{height: 600}}><CircularProgress mode="indeterminate" size={80} /></div>);
 		}
-		if( url === false ) {
-			return (<div className='jazz-report-chart-table-wrapper flex-center' style={{height: 600}}><b>{'无效模板'}</b></div>);
-		}
+		// if( url === false ) {
+		// 	return (<div className='jazz-report-chart-table-wrapper flex-center' style={{height: 600}}><b>{'无效模板'}</b></div>);
+		// }
 
 		return (
 			<div className='jazz-report-chart-table-wrapper'>
 				<div className='jazz-report-chart-table-header'>
-					<ul className='jazz-report-chart-table-header-action'>
+					<ul className='jazz-report-chart-table-header-action' style={{
+						width: url ? false : 100
+					}}>
 						{isFull() && <li><LinkButton onClick={() => onEdit(id)} label={'设置'}/></li>}{isFull() && '|'}
-						{isFull() && <li><LinkButton onClick={() => onDelete(id)} label={'删除'}/></li>}{isFull() && '|'}
-						<li><LinkButton onClick={() => onSetFirst(id)} label={'设为首个报表'}/></li>
+						{isFull() && <li><LinkButton onClick={() => onDelete(id)} label={'删除'}/></li>}{isFull() && url && '|'}
+						<li>{url && <LinkButton onClick={() => onSetFirst(id)} label={'设为首个报表'}/>}</li>
 					</ul>
 					<div>{data.get('Name')}</div>
-					<LinkButton onClick={() => onDownload(id)} iconName='download-icon' label={'下载'}/>
+					<div>{url && <LinkButton onClick={() => onDownload(id)} iconName='download-icon' label={'下载'}/>}</div>
 				</div>
-				<iframe 
+				{url ? <iframe 
 					src={`https://view.officeapps.live.com/op/view.aspx?src=${encodeURIComponent(url)}`}
 					border="0" 
 					height='600'
-					width='100%'/>
+					width='100%'/> :
+					<div style={{height: 600}} className='flex-center'><b>{'无效模板'}</b></div>}
 			</div>
 		);
 	}
