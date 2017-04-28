5c6501b557b10dda9eef54cedfac415912492281
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2017-04-28 16:38:51 +0800

    update for GUI

--- src/app/components/ECM/MeasuresItem.jsx
+++ src/app/components/ECM/MeasuresItem.jsx
@@ -10,10 +10,10 @@ function getUrl(url) {
 	//return "url(" + Config.ServeAddress + url+")";
 }
 function validValue(value) {
-	return value!==null?`${CommonFuns.getLabelData(value*1)} RMB`:'- RMB';
+	return value!==null?CommonFuns.getLabelData(value*1):'-';
 }
 
-class IconText extends Component{
+export class IconText extends Component{
   render(){
     return(
       <div style={{marginLeft:'50px'}}>
@@ -21,7 +21,11 @@ class IconText extends Component{
           {this.props.icon}
           <div style={{fontSize:'12px',marginLeft:'5px'}}>{this.props.label}</div>
         </div>
-        <div style={{marginTop:'5px'}}>{this.props.value}</div>
+				{(this.props.value || this.props.uom) && <div style={{display:'flex',flexDirection:'row',marginTop:'10px',alignItems:'baseline'}}>
+					<div style={{fontSize: '25px',fontWeight: '500',color: '#626469'}}>{this.props.value}</div>
+					<div style={{fontSize: '14px',color: '#626469',marginLeft:'10px'}}>{this.props.uom}</div>
+				</div>}
+				{this.props.children}
       </div>
     )
   }
@@ -30,10 +34,11 @@ class IconText extends Component{
 IconText.propTypes = {
   icon:React.PropTypes.string,
   label:React.PropTypes.bool,
-  value:React.PropTypes.func,
+  value:React.PropTypes.string,
+	uom:React.PropTypes.string,
 };
 
-export default class MeasuresItem extends Component {
+export class MeasuresItem extends Component {
 
     constructor(props) {
       super(props);
@@ -42,24 +47,24 @@ export default class MeasuresItem extends Component {
     getName(){
       var {EnergyProblem,EnergySolution}=this.props.measure.toJS();
       if(EnergySolution.Name!==null){
-        return <div>{EnergySolution.Name}</div>
+        return <div className="measuresItem-title">{EnergySolution.Name}</div>
       }
       else {
         let iconStyle = {
-            fontSize: '16px'
+            fontSize: '14px'
           },
           style = {
             padding: '0px',
-            height: '18px',
-            width: '18px',
-            fontSize: '18px',
+            height: '13.5px',
+            width: '14.5px',
+            fontSize: '14px',
             marginTop:'-5px',
 						marginRight:'5px'
           };
         return <div style={{display:"flex",flexDirection:'row',alignItems:'center',marginTop:'-3px'}}>
-                <FontIcon className="icon-no_ecm" color="red" iconStyle ={iconStyle} style = {style} />
-                <div style={{color:'red'}}>{I18N.Setting.ECM.NoECM}</div>
-                <div style={{color:'#abafae',marginLeft:'5px',fontSize:'14px'}}>{`(${I18N.Setting.ECM.EnergyProblem}:${EnergyProblem.Name})`}</div>
+                <FontIcon className="icon-no_ecm" color="#dc0a0a" iconStyle ={iconStyle} style = {style} />
+                <div style={{color:'#dc0a0a',fontSize:'14px'}}>{I18N.Setting.ECM.NoECM}</div>
+                <div style={{color:'#dc0a0a',marginLeft:'5px',fontSize:'10px'}}>{`(${I18N.Setting.ECM.EnergyProblem}:${EnergyProblem.Name})`}</div>
               </div>
       }
     }
@@ -68,6 +73,7 @@ export default class MeasuresItem extends Component {
       var {EnergySolution,EnergyProblem}=this.props.measure.toJS();
       var {ExpectedAnnualCostSaving,InvestmentAmount}=EnergySolution;
 			var InvestmentReturnCycle=MeasuresStore.getInvestmentReturnCycle(InvestmentAmount,ExpectedAnnualCostSaving);
+
       let iconStyle = {
           fontSize: '16px'
         },
@@ -82,12 +88,13 @@ export default class MeasuresItem extends Component {
           sumIcon=<FontIcon className="icon-investment" iconStyle ={iconStyle} style = {style} />,
           periodIcon=<FontIcon className="icon-roi" iconStyle ={iconStyle} style = {style} />;
       return(
-        <div className="content">
+        <div className="measuresItem-content">
           <div className="side">
             <div className="image" style={{backgroundImage:getUrl(EnergyProblem.ThumbnailUrl)}}></div>
-            <IconText icon={costIcon} label={I18N.Setting.ECM.EstimatedAnnualCostSavings} value={validValue(ExpectedAnnualCostSaving)}/>
-            <IconText icon={sumIcon} label={I18N.Setting.ECM.InvestmentAmount} value={validValue(InvestmentAmount)}/>
-            <IconText icon={periodIcon} label={I18N.Setting.ECM.PaybackPeriod} value={InvestmentReturnCycle || '-'}/>
+            <IconText icon={costIcon} label={I18N.Setting.ECM.EstimatedAnnualCostSavings} value={validValue(ExpectedAnnualCostSaving)} uom="RMB"/>
+            <IconText icon={sumIcon} label={I18N.Setting.ECM.InvestmentAmount} value={validValue(InvestmentAmount)} uom="RMB"/>
+            <IconText icon={periodIcon} label={I18N.Setting.ECM.PaybackPeriod} value={InvestmentReturnCycle || '-'}
+											uom={CommonFuns.isNumber(InvestmentReturnCycle)?I18N.EM.Year:''}/>
             <div style={{marginLeft:'100px'}}>{this.props.personInCharge}</div>
           </div>
           <div className="side">
@@ -99,18 +106,27 @@ export default class MeasuresItem extends Component {
 
     _renderName(){
       var {EnergyProblem}=this.props.measure.toJS();
+			var styles={
+				box:{
+					width: '16px',
+  				height: '16px',
+					color: '#9fa0a4',
+					marginRight:'10px'
+				}
+			}
       return(
         <div className="name">
           <div className="side">
             {this.props.hasCheckBox && <Checkbox disabled={this.props.disabled} checked={this.props.isChecked}
 																									onCheck={this.props.onChecked}
-																									style={{width:'40px'}}
+																									style={{width:'16px'}}
+																									iconStyle={styles.box}
 																									onClick={(e)=>{
 																										e.stopPropagation();
 																									}}/>}
             {this.getName()}
           </div>
-          <div style={{fontSize:'16px'}}>{MeasuresStore.getEnergySys(EnergyProblem.EnergySys)}</div>
+          <div style={{fontSize:'14px',color: '#0f0f0f'}}>{MeasuresStore.getEnergySys(EnergyProblem.EnergySys)}</div>
         </div>
       )
     }
