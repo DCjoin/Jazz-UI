e8bb871e0661aa8b76784fb6db5bf0eeef145393
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2017-05-02 15:40:11 +0800

    update

--- src/app/config/local.jsx
+++ src/app/config/local.jsx
@@ -3,9 +3,9 @@ export default {
   // ServeAddress: "http://sechina.prod35.energymost.com/webapihost",
   // ServeAddress: "http://sp0117.test33.energymost.com/webapihost",
 
-  ServeAddress: "http://10.177.122.150/webapihost",
+  // ServeAddress: "http://10.177.122.150/webapihost",
   // ServeAddress: "http://sp1.test38.energymost.com/webapihost",
-  // ServeAddress: "http://sp1.dev.energymost.com/webapihost",
+  ServeAddress: "http://sp1.dev.energymost.com/webapihost",
   // ServeAddress: "http://localhost:8080",
   APIBasePath: "/API",
   socketServer: "",
