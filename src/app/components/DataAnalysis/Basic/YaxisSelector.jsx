19aef02923a1357648e6313b301d5226ebccf543
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2017-05-17 17:03:07 +0800

    update for YaxisSelector

--- src/app/components/DataAnalysis/Basic/YaxisSelector.jsx
+++ src/app/components/DataAnalysis/Basic/YaxisSelector.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import {Dialog, DropDownMenu, TextField, FontIcon, IconButton} from 'material-ui';
 import NewDialog from 'controls/NewDialog.jsx';
 import FlatButton from 'controls/FlatButton.jsx';
+import NewFlatButton from 'controls/NewFlatButton.jsx';
 import MenuItem from 'material-ui/MenuItem';
 import CommonFuns from 'util/Util.jsx';
 import _ from 'lodash';
@@ -197,23 +198,21 @@ var YaxisDialog = React.createClass({
   },
   render() {
     var _buttonActions = [
-      <FlatButton
-      label={I18N.Common.Button.Save}
+      <NewFlatButton
+      label={I18N.Common.Button.Cancel2}
       secondary={true}
-      onClick={this._onDialogSubmit} />,
-      <FlatButton
-      label={I18N.Common.Button.Cancel}
-      primary={true}
       onClick={this._onDialogCancel} style={{
-        marginRight: '420px'
+        marginRight: '20px',width:'68px',minWidth:'68px'
+      }}/>,
+      <NewFlatButton
+      label={I18N.Common.Button.Save}
+      primary={true}
+      onClick={this._onDialogSubmit}
+      style={{
+        width:'68px',
+        minWidth:'68px'
       }}/>,
 
-      <FlatButton secondary={true} style={{
-        marginRight: '20px'
-      }} onClick={this._onRemoveAllClick}>
-               <FontIcon className="fa fa-trash-o"/>
-               <span>{I18N.Common.Button.ClearAll}</span>
-            </FlatButton>
     ];
 
     var yaxisConfig = this.props.yaxisConfig;
@@ -222,12 +221,17 @@ var YaxisDialog = React.createClass({
 
     for (var key in yaxisConfig) {
       var uom = key; //== 'undefined' ? '' : key;
-      let styleObj = {};
-      if (i > 1) {
-        styleObj = {
-          marginTop: '40px'
-        };
-      }
+      let styleObj = {
+        padding:'15px 0',
+        borderTop:'1px solid #e6e6e6',
+        fontSize: '14px',
+        color:'#626469',
+      };
+      // if (i > 1) {
+      //   styleObj = {
+      //     marginTop: '40px'
+      //   };
+      // }
 
       let defaultValues = null;
       let storedConfigItem = this.getStoredConfigItemByUOM(uom, this.props.storedConfig);
@@ -236,15 +240,12 @@ var YaxisDialog = React.createClass({
       }
 
       let group = <div style = {styleObj}>
+        <div> {I18N.EM.YAxisTitle + i} </div>
         <div style={{
-        fontSize: '14px',
-        marginBottom: '18px'
-      }}> {I18N.EM.YAxisTitle + i} </div>
-        <div style={{
-        marginBottom: '40px'
+        marginTop: '10px'
       }}>
           <span style={{
-        width: '100px',
+        marginRight: '15px',
         display: 'inline-block'
       }}>{I18N.EM.YAxisSettingTags}</span>
           <span style={{
@@ -257,17 +258,25 @@ var YaxisDialog = React.createClass({
       groups.push(group);
     }
     let titleStyle ={
-      fontSize: '20px',
+      fontSize: '16px',
+      color:'#0d0d0d',
       fontWeight: 'bold',
-      padding: '24px 0 0 25px'
+      padding: '0',
+      height:'72px',
+      lineHeight:'72px',
+      margin:'0 30px'
     };
-    var dialog = <NewDialog title={I18N.EM.YAxisSetting} titleStyle={titleStyle} actions={_buttonActions}
-    modal={true} ref="dialogWindow" onDismiss={this.onYaxisDialogDismiss} open={this.state.show}>
-      <div className={'jazz-energy-yaxis-container'} style={{
-      marginLeft: '26px',
-      'overflow-y': 'auto',
-      maxHeight: '300px'
-    }}>
+    var dialog = <NewDialog title={I18N.EM.YAxisSetting} titleStyle={titleStyle} actions={_buttonActions} actionsContainerStyle={{marginLeft:'30px'}}
+    modal={true} ref="dialogWindow" onDismiss={this.onYaxisDialogDismiss} open={this.state.show} contentStyle={{position:'relative',marginLeft:'30px'}}>
+    <NewFlatButton secondary={true} style={{
+      width:'110px',
+      border:'none',
+      right:'0',
+      position:'absolute',
+      color:'#32ad3d',
+      top:'-49px'
+    }} onClick={this._onRemoveAllClick} label={I18N.Common.Button.ClearAll} icon={<FontIcon className="icon-delete" style={{fontSize:'14px'}}/>}/>
+      <div className={'jazz-energy-yaxis-container'}>
         {groups}
       </div>
     </NewDialog>;
@@ -385,17 +394,19 @@ var MaxMinPair = React.createClass({
   render() {
     return <div>
          <div style={{display:'flex',alignItems:'center'}}> <span style={{
-        width: '100px',
-        display: 'inline-block'
+        marginRight: '15px',
+        display: 'inline-block',
       }}>{I18N.Common.Glossary.Max}</span>
     <TextField hintText={I18N.Common.Glossary.Auto} value={this.state.maxValue} errorText={this.state.maxError}
+      hintStyle={{fontSize:'12px',color:'#9fa0a4'}}
       onChange={this._onMaxFieldChange} ref={this.props.uom + '_max'} value={this.state.maxValue}/>
                <span>{this.props.uom}</span></div>
          <div style={{display:'flex',alignItems:'center'}}> <span style={{
-        width: '100px',
-        display: 'inline-block'
+        marginRight: '15px',
+        display: 'inline-block',
       }}>{I18N.Common.Glossary.Min}</span>
     <TextField hintText={I18N.Common.Glossary.Auto} value={this.state.minValue} errorText={this.state.minError}
+      hintStyle={{fontSize:'12px',color:'#9fa0a4'}}
       onChange={this._onMinFieldChange} ref={this.props.uom + '_min'} value={this.state.minValue}/>
                <span>{this.props.uom}</span></div>
        </div>;
