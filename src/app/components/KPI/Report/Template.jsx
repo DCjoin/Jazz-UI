62d5f5487268a20c90044a6611de74aabee8ce25
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2017-06-14 09:24:45 +0800

    update for templat maneger privilege change

--- src/app/components/KPI/Report/Template.jsx
+++ src/app/components/KPI/Report/Template.jsx
@@ -48,11 +48,11 @@ var Template = React.createClass({
   },
   _getOnlyRead(rivilege) {
     var onlyRead = false;
-    // if (rivilege !== null) {
-    //   if ((rivilege.indexOf(PermissionCode.DATA_REPORT_MANAGEMENT.READONLY+'') > -1) && (rivilege.indexOf(PermissionCode.DATA_REPORT_MANAGEMENT.FULL+'') === -1)) {
-    //     onlyRead = true;
-    //   }
-    // }
+    if (rivilege !== null) {
+      if ((rivilege.indexOf(PermissionCode.INDEX_AND_REPORT.READONLY+'') > -1) && (rivilege.indexOf(PermissionCode.INDEX_AND_REPORT.FULL+'') === -1)) {
+        onlyRead = true;
+      }
+    }
     return onlyRead;
   },
 
