94d001b1894e578cf9f2936e2a02defa0210be14
Lawrence <liang-lawrence.liu@schneider-electric.com>
2017-07-25 12:04:12 +0800

    fix bug

--- src/app/components/Diagnose/CreateDiagnose.jsx
+++ src/app/components/Diagnose/CreateDiagnose.jsx
@@ -817,7 +817,7 @@ function ModelCCondition({
 				style={{fontSize: 14}}
 				regex={/^(\-?)\d{1,9}([.]\d{1,6})?$/}
 				errorMessage={I18N.Setting.Diagnose.FormatVaildTip}
-				hintText={I18N.Setting.Diagnose.AssociateValue}
+				hintText={I18N.Setting.Diagnose.InputAssociateValue}
 				defaultValue={AssociateValue}
 				didChanged={onUpdateAssoicateValue}/>
 		</div>
@@ -851,7 +851,7 @@ function ModelCCondition({
 				style={{fontSize: 14}}
 				regex={/^(\-?)\d{1,9}([.]\d{1,6})?$/}
 				errorMessage={I18N.Setting.Diagnose.FormatVaildTip}
-				hintText={I18N.Setting.Diagnose.TriggerValue}
+				hintText={I18N.Setting.Diagnose.InputTriggerValue}
 				defaultValue={TriggerValue}
 				didChanged={onUpdateTriggerValue}/>
 		</div>
@@ -1054,11 +1054,11 @@ export class CreateStep2 extends Component {
 			AssociateTag,
 			...other,
 		} = this.props,
-		disabledPreview = step2NeedRequire(DiagnoseModel, TriggerType, TriggerValue, AssociateTag.TriggerValue);
+		disabledPreview = step2NeedRequire(DiagnoseModel, TriggerType, TriggerValue, AssociateTag && AssociateTag.TriggerValue);
 		let _firstUom, _lasttUom;
 		if(chartData) {
 			_firstUom = getUomById(chartData.getIn(['EnergyViewData', 'TargetEnergyData', 0, 'Target', 'UomId'])).Code;
-			
+
 			if( isTypeC(DiagnoseModel) ) {
 				_lasttUom = getUomById(
 					chartData.getIn([
@@ -1132,8 +1132,8 @@ export class CreateStep2 extends Component {
 					ConditionType={ConditionType}
 					onUpdateConditionType={onUpdateFilterObj('ConditionType')}
 
-					AssociateType={AssociateTag.ConditionType}
-					AssociateValue={AssociateTag.TriggerValue}
+					AssociateType={AssociateTag && AssociateTag.ConditionType}
+					AssociateValue={AssociateTag && AssociateTag.TriggerValue}
 					onUpdateAssoicateType={(val) => {
 						onUpdateFilterObj('AssociateTag')({...AssociateTag, ...{
 							ConditionType: val
@@ -1299,7 +1299,7 @@ class CreateDiagnose extends Component {
 				DiagnoseItemId: this.props.DiagnoseItem.get('Id'),
 				EnergyLabelId: this.props.EnergyLabel.get('Id'),
 				DiagnoseModel: this.props.EnergyLabel.get('DiagnoseModel'),
-				TagIds: checkedTags.map(tag => tag.Id).concat(checkedAssociateTag.map(tag => tag.Id)),
+				TagIds: checkedTags.map(tag => tag.Id),
 			}}, this.state.chartData);
 		}
 		this.setState({
