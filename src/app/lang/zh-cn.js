94f4c3b73054bd1c33fde7e6e8be57d0a00cc15c
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2017-08-31 17:36:24 +0800

    fix bug #18509

--- src/app/lang/zh-cn.js
+++ src/app/lang/zh-cn.js
@@ -2609,7 +2609,6 @@ I18N.SaveEffect.ByYear = '年度节能效果';
 I18N.SaveEffect.OrderByCommo = '按介质展示';
 I18N.SaveEffect.OrderByBuilding = '按建筑展示';
 
-I18N.SaveEffect.Best = {};
 
 
 module.exports = I18N;
