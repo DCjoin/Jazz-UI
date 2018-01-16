1bc4113bf5554bbac7902563797415780ecc6101
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2018-01-16 14:23:47 +0800

    change for test

--- src/server/release_server.js
+++ src/server/release_server.js
@@ -180,7 +180,7 @@ app.get('/:lang/spinitsso-redirect', (req, res) => {
   redirectURL.pathname = req.params.lang + redirectURL.pathname;
   console.log(redirectURL.pathname);
   let spDomain = req.hostname.split(".")[0] ? req.hostname.split(".")[0] : "";
-  spDomain=spDomain==='dev'?'sp1':spDomain;
+  // spDomain=spDomain==='dev'?'sp1':spDomain;
   //因为sso的dev环境存在问题，暂时都指向sp1环境
   // let spDomain = 'sp1';
   console.log(spDomain);
