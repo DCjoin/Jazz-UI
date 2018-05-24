5c0215fe418ff76c40c247785293ee0aa0c52308
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2018-05-24 15:23:32 +0800

    fix bug #20882

--- src/app/components/ECM/edit_solution.jsx
+++ src/app/components/ECM/edit_solution.jsx
@@ -138,7 +138,7 @@ export default class EditSolution extends Component {
 
         if( paths.join('') === 'ProblemSolutionTitle' ) {
           if( !value ) {
-            error = I18N.Setting.ECM.TitleErrorTip;
+            error = I18N.Setting.Diagnose.PleaseInput+I18N.Setting.Diagnose.SolutionTitle;
           }
         }else if( paths.join('') === 'ProblemEnergySys' ) {
           if( !value && !this.state.solution.getIn(paths)) {
