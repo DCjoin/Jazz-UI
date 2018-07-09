af35f507003eb6dce7ffcabc9b253733f7667fe3
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2018-07-09 18:13:58 +0800

    fix bug #1903

--- src/app/components/DataQualityMaintenance/summary_chart.jsx
+++ src/app/components/DataQualityMaintenance/summary_chart.jsx
@@ -204,13 +204,13 @@ class ChartComponent extends Component {
           var content='';
           var j2d=CommonFuns.DataConverter.JsonToDateTime;
           var data=that.props.data.getIn([this.point.index]);
-          var {Time,NormalNodes,AbnormalNodes}=data.toJS();
+          var {Time,NormalNodes,AbnormalNodes,IsNormal}=data.toJS();
           return `
           <div>
                   <div style="font-size:14px;color:#626469">${CommonFuns.formatDateByStep(j2d(Time,true),null,null,6)}</div>
                     <div style="display:flex">
                       <div style="font-size:12px;color:#626469">${that.props.name}${I18N.VEE.Summary+'：'}</div>
-                      <div style="font-size:12px;color:#11d9db">${I18N.VEE.normal}</div>
+                      <div style="font-size:12px;color:${IsNormal?'#11d9db':'#f46a58'}">${IsNormal?I18N.VEE.normal:I18N.VEE.abnormal}</div>
                     </div>
                     <div style="display:flex">
                       <div style="font-size:12px;color:#626469">${I18N.format(I18N.VEE.SummaryTooltip1,NormalNodes+AbnormalNodes)}${I18N.VEE.SummaryTooltip2}</div>
