a5560a2d475760a4f8067374977ed8a98043606d
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2018-07-11 13:20:07 +0800

    fix bug #1952

--- src/app/controls/ViewableDatePicker.jsx
+++ src/app/controls/ViewableDatePicker.jsx
@@ -197,13 +197,14 @@ class DatePicker extends React.Component {
     }
 
     var style = {width: this.props.width || 430,
-      height: '30px',display: 'flex',justifyContent: 'center',alignItems: 'center'}
+      height: '30px',display: 'flex',justifyContent: 'center',alignItems: 'center',
+    backgroundColor:this.props.disabled?'#f2f2f2':'#ffffff'}
 
     return (
       <div className={this.props.datePickerClassName}>
-        {this.props.isDateEdited?this._renderTextField(error):<div className="jazz-viewableDatePicker jazz-viewableTextField" style={style} onClick={()=>{this.setState({popup:true})}}>
+        {this.props.isDateEdited?this._renderTextField(error):<div className="jazz-viewableDatePicker jazz-viewableTextField" style={style} onClick={!this.props.disabled?()=>{this.setState({popup:true})}:()=>{}}>
                 <div className="jazz-viewable-title" >{this.props.title}</div>
-                <div style={{fontSize:'14px',color:'#626469'}}>{this.props.value}</div>
+                <div style={{fontSize:'14px',color:this.props.disabled?'#c0c0c0':'#626469'}}>{this.props.value}</div>
               </div>}
         {this._renderPopup(dateObject)}
       </div>
