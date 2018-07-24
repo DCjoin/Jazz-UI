2e6bbcda0c27aece76529e00165f71e33b804ecc
Rea WANG <SESA378106@schneider-electric.com>
2018-07-24 13:54:48 +0800

    fix bug #2325

--- src/app/components/DataQualityMaintenance/panel_right.jsx
+++ src/app/components/DataQualityMaintenance/panel_right.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-
+import DataQualityMaintenanceAction from 'actions/data_quality_maintenance.jsx';
 import DataQuality from 'constants/actionType/data_quality.jsx';
 import Panel from 'controls/toggle_icon_panel.jsx';
 import TagContentField from './tag_content_field.jsx';
 import SummaryContentField from './summary_content_field.jsx';
 import BuildingContentField from './building_content_field.jsx';
 import Organization from './organization.jsx'
-
+import CurrentUserStore from 'stores/CurrentUserStore.jsx';
+import moment from 'moment';
 export default class Right extends Component {
+  _changeReadStatus(nodeData,filterType){
+    if(nodeData && nodeData.get("IsNotRead")){
+      DataQualityMaintenanceAction.updatereadstatus( {"Id": nodeData.get('Id'),
+                                                      "NodeType": nodeData.get('NodeType'),
+                                                      "UpdateTime": moment().utc().format('YYYY-MM-DDTHH:mm:ss'),
+                                                      "IsNotRead": false,
+                                                      "UserId": CurrentUserStore.getCurrentUser().Id,
+                                                      "ExceptionType": filterType},nodeData);
+    }
+  }
+  componentDidMount(){
+    this._changeReadStatus(this.props.selectedNode,this.props.filterType)
+  }
+  componentWillReceiveProps(nextProps){
+    this._changeReadStatus(nextProps.selectedNode,nextProps.filterType)
+  }
   render() {
     let { selectedNode ,showLeft, onToggle ,filterType, startTime, endTime} = this.props;
-    console.log(DataQuality.nodeType, 'DataQuality.nodeType')
     if( selectedNode ){
       // TagContentField 数据点，SummaryContentField网关 ,selectedNode.get("NodeType")是5,6等
 
